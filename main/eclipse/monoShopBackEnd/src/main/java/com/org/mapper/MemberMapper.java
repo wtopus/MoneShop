134cd0a4ca285da28a1977eabc501c0598e9b51e
wtopus <cake3717@gmail.com>
2022-05-31 12:29:29 +0900

    LoginController update

--- main/eclipse/monoShopBackEnd/src/main/java/com/org/mapper/MemberMapper.java
+++ main/eclipse/monoShopBackEnd/src/main/java/com/org/mapper/MemberMapper.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Select;
 
 import com.org.domain.MemberVO;
+import com.org.domain.ShopCartVO;
 
 
 public interface MemberMapper {
 	/* @Select("select * from member ") */
 	public List<MemberVO> getList();
 	public MemberVO isLogin(MemberVO memberVO);
+	
+	public boolean isEmpty(String mid, Integer pno);
+	
+	public List<ShopCartVO> shopcartList(String mid);
 }
--- main/eclipse/monoShopBackEnd/src/main/java/com/org/service/MoneService.java
+++ main/eclipse/monoShopBackEnd/src/main/java/com/org/service/MoneService.java
@@ -1,7 +1,14 @@
 package com.org.service;
 
+import java.util.List;
+
 import com.org.domain.MemberVO;
+import com.org.domain.ShopCartVO;
 
 public interface MoneService {
 	public MemberVO isLogin(MemberVO memberVO);
+	
+	public boolean isEmpty(String mid, Integer pno);
+	
+	public List<ShopCartVO> shopcartList(String mid);
 }
